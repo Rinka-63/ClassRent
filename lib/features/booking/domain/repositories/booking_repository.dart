87dac391be193a9c0052944b3e0933f9263c19d1
garet132 <garetml123@gmail.com>
2026-07-07 23:05:55 +0700

    mengubah booking_repository.dart

--- lib/features/booking/domain/repositories/booking_repository.dart
+++ lib/features/booking/domain/repositories/booking_repository.dart
@@ -9,7 +9,9 @@ abstract interface class BookingRepository {
   Future<Either<Failure, List<Booking>>> getBookingsForUser(String userId);
   Future<Either<Failure, Booking>> getBookingById(String id);
   Future<Either<Failure, Booking>> createBooking(Map<String, dynamic> payload);
-  Future<Either<Failure, Booking>> updateBooking(String id, Map<String, dynamic> payload);
+  Future<Either<Failure, Booking>> updateBooking(
+      String id, Map<String, dynamic> payload);
   Future<Either<Failure, Unit>> cancelBooking(String id);
   Future<Either<Failure, Unit>> deleteBooking(String id);
+  Future<Either<Failure, Unit>> expirePastBookings();
 }
