f90394f450af3d33b151fa174125b9b57b775a34
garet132 <garetml123@gmail.com>
2026-07-07 23:05:45 +0700

    mengubah agency_repository.dart

--- lib/features/admin/domain/repositories/agency_repository.dart
+++ lib/features/admin/domain/repositories/agency_repository.dart
@@ -2,6 +2,7 @@ import 'package:fpdart/fpdart.dart';
 
 import '../../../../core/error/failure.dart';
 import '../entities/agency.dart';
+import '../entities/agency_withdrawal.dart';
 import '../entities/platform_stats.dart';
 import '../entities/platform_user.dart';
 
@@ -12,4 +13,14 @@ abstract interface class AgencyRepository {
   Future<Either<Failure, Unit>> approveAgency(String agencyId);
   Future<Either<Failure, Unit>> rejectAgency(String agencyId);
   Future<Either<Failure, Unit>> setAgencyActive(String agencyId, bool isActive);
+  Future<Either<Failure, List<AgencyWithdrawal>>> getMyWithdrawals(
+    String adminId,
+  );
+  Future<Either<Failure, Unit>> createWithdrawal({
+    required String adminId,
+    required double amount,
+    required String bankName,
+    required String accountName,
+    required String accountNumber,
+  });
 }
